05d932ba9e6bcb862e5874de4ef3eaa8d54a2245
Drew Holzworth <drew@holzworth.info>
2023-01-31 14:06:55 +1100

    Stop using reflection-based deserializer
    (as this won't work with native aot)

--- src/Models/RunSettings.cs
+++ src/Models/RunSettings.cs
@@ -1,6 +1,3 @@
-using LpjGuess.Runner.Models.Raw;
-using Tomlyn.Helpers;
-
 namespace LpjGuess.Runner.Models;
 
 /// <summary>
@@ -99,72 +96,5 @@ public class RunSettings
 		EmailNotifications = emailNotifications;
 		EmailAddress = emailAddress;
 		JobName = jobName;
-	}
-
-	/// <summary>
-	/// Attempt to create a new <see cref="RunSettings"/> instance from the raw
-	/// user input object. This may throw if user inputs are invalid.
-	/// </summary>
-	/// <param name="model">User input.</param>
-	public RunSettings(InputModel model)
-	{
-		DryRun = model.DryRun;
-		EmailNotifications = model.EmailNotifications;
-
-		if (model.CpuCount == 0)
-			throw new InvalidOperationException(VarNotSet(nameof(CpuCount)));
-		CpuCount = model.CpuCount;
-
-		if (model.Memory == 0)
-			throw new InvalidOperationException(VarNotSet(nameof(Memory)));
-		Memory = model.Memory;
-
-		if (string.IsNullOrEmpty(model.OutputDirectory))
-			throw new InvalidOperationException(VarNotSet(nameof(OutputDirectory)));
-		OutputDirectory = model.OutputDirectory;
-
-		if (string.IsNullOrEmpty(model.GuessPath))
-			throw new InvalidOperationException(VarNotSet(nameof(GuessPath)));
-		GuessPath = model.GuessPath;
-
-		if (string.IsNullOrEmpty(model.InputModule))
-			throw new InvalidOperationException(VarNotSet(nameof(InputModule)));
-		InputModule = model.InputModule;
-
-		if (string.IsNullOrEmpty(model.Walltime))
-			throw new InvalidOperationException(VarNotSet(nameof(Walltime)));
-		if (!TimeSpan.TryParseExact(model.Walltime, "c", null, out TimeSpan walltime))
-			throw new InvalidOperationException($"Invalid walltime value: '${model.Walltime}' in input file");
-		Walltime = walltime;
-
-		if (string.IsNullOrEmpty(model.Queue))
-			throw new InvalidOperationException(VarNotSet(nameof(Queue)));
-		Queue = model.Queue;
-
-		if (string.IsNullOrEmpty(model.Project))
-			throw new InvalidOperationException(VarNotSet(nameof(Project)));
-		Project = model.Project;
-
-		if (model.EmailNotifications && string.IsNullOrEmpty(model.EmailAddress))
-			throw new InvalidOperationException(VarNotSet(nameof(EmailAddress)));
-
-		// If EmailNotifications is false, then we can use an empty string here,
-		// as EmailAddress won't be used.
-		EmailAddress = model.EmailAddress ?? "";
-
-		if (string.IsNullOrEmpty(model.JobName))
-			throw new InvalidOperationException(VarNotSet(nameof(JobName)));
-		JobName = model.JobName;
-	}
-
-	/// <summary>
-	/// Return an appropriate error message for when a variable is not set in
-	/// the input file.
-	/// </summary>
-	/// <param name="variable">Name of the variable.</param>
-	private string VarNotSet(string variable)
-	{
-		string name = TomlNamingHelper.PascalToSnakeCase(variable);
-		return $"Variable '{name}' is not set in input file";
 	}
 }
--- src/Parsers/TomlParser.cs
+++ src/Parsers/TomlParser.cs
@@ -1,7 +1,9 @@
+using System.Globalization;
 using LpjGuess.Runner.Extensions;
 using LpjGuess.Runner.Models;
-using LpjGuess.Runner.Models.Raw;
 using Tomlyn;
+using Tomlyn.Helpers;
+using Tomlyn.Model;
 
 namespace LpjGuess.Runner.Parsers;
 
@@ -15,7 +17,7 @@ internal class TomlParser : IParser
 	{
 		try
 		{
-			InputModel model = Toml.ToModel<InputModel>(File.ReadAllText(file));
+			TomlTable model = Toml.ToModel(File.ReadAllText(file));
 			return Parse(model);
 		}
 		catch (Exception error)
@@ -29,14 +31,82 @@ internal class TomlParser : IParser
 	/// some manual validation of inputs not covered by the library.
 	/// </summary>
 	/// <param name="model">The raw user input object.</param>
-	private Instructions Parse(InputModel model)
+	private Instructions Parse(TomlTable model)
 	{
-		if (model.Insfiles.Count == 0)
-			throw new InvalidOperationException($"No instruction files provided");
+		IReadOnlyCollection<Factorial> combinations = ParseParameters(model);
+		RunSettings settings = ParseRunSettings(model);
+		IReadOnlyCollection<string> pfts = ParsePfts(model);
+		IReadOnlyCollection<string> insFiles = ParseInsFiles(model);
 
-		IReadOnlyCollection<Factorial> combinations = GetParameters(model.Parameters);
-		RunSettings settings = new RunSettings(model);
-		return new Instructions(model.Insfiles, model.Pfts, combinations, settings);
+		return new Instructions(insFiles, pfts, combinations, settings);
+	}
+
+	private IReadOnlyCollection<string> ParseInsFiles(TomlTable model)
+	{
+		const string key = "insfiles";
+		return ParseStringArray(model, key, false);
+	}
+
+	private IReadOnlyCollection<string> ParsePfts(TomlTable model)
+	{
+		const string key = "pfts";
+		return ParseStringArray(model, key, true);
+	}
+
+	private RunSettings ParseRunSettings(TomlTable model)
+	{
+		bool dryRun = ParseBool(model, "dry_run", optional: true);
+
+		uint cpuCount = ParseUint(model, "cpu_count");
+		uint memory = ParseUint(model, "memory");
+		TimeSpan walltime = ParseTimeSpan(model, "walltime");
+
+		string queue = ParseString(model, "queue");
+		string project = ParseString(model, "project");
+		string jobName = ParseString(model, "job_name");
+
+		string outputDirectory = ParseString(model, "output_directory");
+		string guessPath = ParseString(model, "guess_path");
+		string inputModule = ParseString(model, "input_module");
+
+		// Email address is optional iff emailNotifications is false.
+		bool emailNotifications = ParseBool(model, "email_notifications");
+		string emailAddress = ParseString(model, "email_address", !emailNotifications);
+
+		return new RunSettings(
+			dryRun,
+			outputDirectory,
+			guessPath,
+			inputModule,
+			cpuCount,
+			walltime,
+			memory,
+			queue,
+			project,
+			emailNotifications,
+			emailAddress,
+			jobName
+		);
+	}
+
+	/// <summary>
+	/// Parse all factorial settings from the model.
+	/// </summary>
+	/// <param name="model">The model.</param>
+	private IReadOnlyCollection<Factorial> ParseParameters(TomlTable model)
+	{
+		const string keyParameters = "parameters";
+		if (!model.ContainsKey(keyParameters))
+			return new List<Factorial>();
+
+		if ( !(model[keyParameters] is TomlTable section) )
+			throw new InvalidOperationException($"{keyParameters} should be a section");
+
+		Dictionary<string, List<string>> parameters = new Dictionary<string, List<string>>();
+		foreach ((string key, object value) in section)
+			parameters[key] = ParseStringArray(section, key).ToList();
+
+		return GetParameters(parameters);
 	}
 
 	/// <summary>
@@ -56,5 +126,139 @@ internal class TomlParser : IParser
 		// Return all combinations thereof.
 		List<List<Factor>> combinations = factors.AllCombinations();
 		return combinations.Select(c => new Factorial(c)).ToList();
+	}
+
+	/// <summary>
+	/// Parse a string array from the model. If the key doesn't exist, an
+	/// exception will be thrown, unless optional is set to true, in which case
+	/// an empty list will be returned. This function will also throw if the
+	/// array is empty (unless optional is true).
+	/// </summary>
+	/// <param name="model">The model.</param>
+	/// <param name="keyName">Name of the string array.</param>
+	/// <param name="optional">Is the array optional?</param>
+	private IReadOnlyCollection<string> ParseStringArray(TomlTable model, string keyName, bool optional = false)
+	{
+		if (!model.ContainsKey(keyName))
+			throw new InvalidOperationException(VarNotSet(keyName));
+
+		// TBI - add support for scalar as single-element list.
+		if ( !(model[keyName] is TomlArray array) )
+			throw new InvalidOperationException($"{keyName} must be an array");
+
+		if (array.Count == 0 && !optional)
+			throw new InvalidOperationException($"{keyName} is empty");
+
+		List<string> values = array
+			.Select(i => i?.ToString())
+			.Where(i => !string.IsNullOrEmpty(i))
+			.Cast<string>()
+			.Select(i => i.Trim())
+			.ToList();
+
+		if (values.Count == 0 && !optional)
+			throw new InvalidOperationException($"No values provided for {keyName}");
+
+		return values;
+	}
+
+	/// <summary>
+	/// Parse a string value from the model. If the key doesn't exist, an
+	/// exception will be thrown, unless optional is set to true, in which case
+	/// an empty string will be returned.
+	/// </summary>
+	/// <param name="model">Toml model.</param>
+	/// <param name="key">Key name.</param>
+	/// <param name="optional">Is this value optional?</param>
+	private string ParseString(TomlTable model, string key, bool optional = false)
+	{
+		if (!model.ContainsKey(key))
+		{
+			if (optional)
+				// Return default value.
+				return string.Empty;
+			else
+				throw new InvalidOperationException($"{key} not set");
+		}
+
+		if ( !(model[key] is string result) )
+			throw new InvalidOperationException($"{key} must be a string (ie quoted) value");
+		return result;
+	}
+
+	/// <summary>
+	/// Parse a <see cref="TimeSpan"/> from the model.
+	/// </summary>
+	/// <param name="model">The model.</param>
+	/// <param name="key">Name of the timespan.</param>
+	private TimeSpan ParseTimeSpan(TomlTable model, string key)
+	{
+		string str = ParseString(model, key);
+		if (!TimeSpan.TryParseExact(str, "c", null, out TimeSpan result))
+			throw new InvalidOperationException($"Invalid timespan value: '{str}' in input file");
+		return result;
+	}
+
+	/// <summary>
+	/// Parse a boolean value from the input file with the given key name. If
+	/// the key does not exist, an exception will be thrown, unless optional is
+	/// set to true, in which case false will be returned.
+	/// </summary>
+	/// <param name="model">Parsed input object.</param>
+	/// <param name="keyName">Key name.</param>
+	/// <param name="optional">Is this parameter optional?</param>
+	private bool ParseBool(TomlTable model, string keyName, bool optional = false)
+	{
+		if (!model.ContainsKey(keyName))
+		{
+			if (optional)
+				// Return default value.
+				return false;
+			else
+				throw new InvalidOperationException(VarNotSet(keyName));
+		}
+
+		object value = model[keyName];
+		if (!(value is bool result))
+			throw new InvalidOperationException($"{keyName} must be boolean (true/false)");
+
+		return result;
+	}
+
+	/// <summary>
+	/// Parse uint value from the input file with the given key name. If the key
+	/// does not exist, an exception will be thrown, unless optional is set to
+	/// true, in which case false will be returned.
+	/// </summary>
+	/// <param name="model">Parsed input object.</param>
+	/// <param name="keyName">Key name.</param>
+	/// <param name="optional">Is this parameter optional?</param>
+	private uint ParseUint(TomlTable model, string keyName, bool optional = false)
+	{
+		if (!model.ContainsKey(keyName))
+		{
+			if (optional)
+				// Return default value.
+				return 0;
+			else
+				throw new InvalidOperationException(VarNotSet(keyName));
+		}
+
+		string? value = model[keyName].ToString();
+		if (value == null || !uint.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out uint result))
+			throw new InvalidOperationException($"{keyName} must be uint (0..{uint.MaxValue})");
+
+		return result;
+	}
+
+	/// <summary>
+	/// Return an appropriate error message for when a variable is not set in
+	/// the input file.
+	/// </summary>
+	/// <param name="variable">Name of the variable.</param>
+	private string VarNotSet(string variable)
+	{
+		string name = TomlNamingHelper.PascalToSnakeCase(variable);
+		return $"Variable '{name}' is not set in input file";
 	}
 }
